c9d8e003c4bf45ed90daac1e4f7beaab153240b1
AvianJay <48914361+AvianJay@users.noreply.github.com>
2021-06-17 09:58:19 +0800

    add curl and something...

--- launcher.bat
+++ launcher.bat
@@ -3,7 +3,7 @@ title Windows command Runner
 
 :check
 echo checking...
-echo 1/3
+echo 1/4
 if exist %USERPROFILE%/wcr/wcr.bat (goto check2) else (goto error)
 
 :error
@@ -18,12 +18,16 @@ echo Please type y or n.
 goto ask
 
 :check2
-echo 2/3
+echo 2/4
 if exist %USERPROFILE%/wcr/com.cfg (goto check3) else (goto cfgset)
 
 :check3
-echo 3/3
-if exist %USERPROFILE%/wcr/resec.cfg (goto ok) else (goto cfgset)
+echo 3/4
+if exist %USERPROFILE%/wcr/resec.cfg (goto check4) else (goto cfgset)
+
+:check4
+echo 4/4
+if exist %USERPROFILE%/wcr/updater.bat (goto ok) else (goto error)
 
 :cfgset
 set /P com="Input command:"
@@ -32,6 +36,25 @@ set /P resec="Repeat times:"
 echo %resec% > %USERPROFILE%/wcr/resec.cfg
 
 :update
+echo Updateing...
 cd %USERPROFILE%
 mkdir wcr
-curl
+cd wcr
+echo Deleteing...
+del wcr.bat
+del updater.bat
+echo Downloading...
+curl https://raw.githubusercontent.com/AvianJay/win-command-runner/main/wcr.bat --output wcr.bat
+curl https://raw.githubusercontent.com/AvianJay/win-command-runner/main/updater.bat --output updater.bat
+echo Updated.Ready to Check.
+ping 127.0.0.1 -n 1 -w 1000 > nul
+goto check1
+
+:ok
+set /P laun="What Do You Want To Do?(l=launch/u=update/s=Set Command and Repeat Time/ul=Update Launcher):"
+if %laun%=l (cd %USERPROFILE%/wcr & start wcr.bat & exit /B)
+if %laun%=u (goto update)
+if %laun%=s (goto cfgset)
+if %laun%=ul (cd %USERPROFILE%/wcr & start updater.bat & exit /B)
+echo Please Type l or u or s or ul.
+goto ok
